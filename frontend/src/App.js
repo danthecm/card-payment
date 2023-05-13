78852f97c3db86acf97f949f714b2832410125d4
danthecm <etchiedaniel1@gmail.com>
2023-05-13 13:47:19 +0100

    [FE-feat] styling main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <div className="app"></div>;
+  return (
+    <main className="main">
+      <div className="container">
+        <h1>Your Payment Details</h1>
+      </div>
+    </main>
+  );
 }
 
 export default App;
