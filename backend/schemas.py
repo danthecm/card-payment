98146bdd7ec307ab1a00bbb126d002de8cf30923
danthecm <etchiedaniel1@gmail.com>
2023-05-13 16:50:30 +0100

    [BE-feat] created card_number custom validator

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,4 +1,6 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, validator
+from datetime import date
+from helpers import validate_length
 
 class PaymentSchema(BaseModel):
     """
@@ -7,5 +9,14 @@ class PaymentSchema(BaseModel):
     name: str
     card_number: int 
     expiry_date: str
-    cvv: int 
+    cvv: int
 
+
+    @validator('card_number')
+    def validate_card_number(cls, value):
+        is_valid = validate_length(value, 16, 19)
+        if not is_valid:
+            raise ValueError("Invalid card number length")
+        return value
+
+ 
