3598e3709fbfb75f855692083545a15e096a9bd1
danthecm <etchiedaniel1@gmail.com>
2023-05-13 20:48:41 +0100

    [FE-feat] style success component and add button to make another payment

--- frontend/src/components/Success/index.js
+++ frontend/src/components/Success/index.js
@@ -1,7 +1,19 @@
-import React from "react";
+import checkIcon from "../../images/check-icon.png";
+import "./Success.css";
 
-const Success = () => {
-  return <div>Success</div>;
+const Success = ({ setPageStatus }) => {
+  const newPaymentHandler = () => {
+    setPageStatus(undefined);
+  };
+  return (
+    <div className="success-container">
+      <img src={checkIcon} alt="check" />
+      <h1>Payment Successful</h1>
+      <button onClick={newPaymentHandler} className="reset-btn">
+        Make another payment
+      </button>
+    </div>
+  );
 };
 
 export default Success;
