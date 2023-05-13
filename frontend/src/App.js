d5a8d80530917b58f6dba9954570948edbbab885
danthecm <etchiedaniel1@gmail.com>
2023-05-13 14:42:14 +0100

    [FE-feat] added PaymentForm component to App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
+import PaymentForm from "./components/PaymentForm";
 
 function App() {
   return (
     <main className="main">
       <div className="container">
         <h1>Your Payment Details</h1>
+        <PaymentForm />
       </div>
     </main>
   );
