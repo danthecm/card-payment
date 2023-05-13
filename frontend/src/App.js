1608730836aa5e9c54983b36a22d4312733ece95
danthecm <etchiedaniel1@gmail.com>
2023-05-13 20:08:05 +0100

    [FE-feat] dynamcially render success, error or paymentForm component in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,24 @@
+import { useState } from "react";
 import "./App.css";
+import Success from "./components/Success";
+import Error from "./components/Error";
 import PaymentForm from "./components/PaymentForm";
 
 function App() {
+  const [pageStatus, setPageStatus] = useState();
   return (
     <main className="main">
       <div className="container">
-        <h1>Your Payment Details</h1>
-        <PaymentForm />
+        {pageStatus?.message === "success" ? (
+          <Success />
+        ) : pageStatus?.message === "error" ? (
+          <Error />
+        ) : (
+          <>
+            <h1>Your Payment Details</h1>
+            <PaymentForm setPageStatus={setPageStatus} />
+          </>
+        )}
       </div>
     </main>
   );
